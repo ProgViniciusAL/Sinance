54d5ec50be98e164cbc69780b989e7b4f2cad393
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2026-04-11 17:35:41 -0300

    fix: Flyway Instances Deleted for improvement soon.

--- src/main/resources/db.migration/V1__User_Table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE if NOT EXISTS user_entity
-(
-    id            UUID NOT NULL,
-    user_name     VARCHAR(255),
-    user_email    VARCHAR(255),
-    password_hash VARCHAR(255),
-    created_at    TIMESTAMP WITHOUT TIME ZONE,
-    CONSTRAINT pk_user_entity PRIMARY KEY (id)
-);
