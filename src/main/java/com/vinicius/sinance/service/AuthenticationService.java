71689031373b42ad89b061e7274d668baa91b51a
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2026-04-11 17:03:55 -0300

    fix: Login and Register method simplified with SOLID principle. fix: Password encode on User Service.

--- src/main/java/com/vinicius/sinance/service/AuthenticationService.java
+++ src/main/java/com/vinicius/sinance/service/AuthenticationService.java
@@ -1,17 +1,37 @@
 package com.vinicius.sinance.service;
 
+import com.vinicius.sinance.dto.auth.LoginRequest;
 import com.vinicius.sinance.model.UserEntity;
+import com.vinicius.sinance.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class AuthenticationService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
 
     public UserEntity getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         assert authentication != null;
         return (UserEntity) authentication.getPrincipal();
+    }
+
+    public String login(LoginRequest loginRequest){
+        var usernameAndPassword = new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
+        var authentication = authenticationManager.authenticate(usernameAndPassword);
+
+        return tokenService.generateToken((UserEntity) Objects.requireNonNull(authentication.getPrincipal()));
     }
 
 }
--- src/main/java/com/vinicius/sinance/service/UserService.java
+++ src/main/java/com/vinicius/sinance/service/UserService.java
@@ -1,16 +1,24 @@
 package com.vinicius.sinance.service;
 
+import com.vinicius.sinance.dto.auth.RegisterRequest;
+import com.vinicius.sinance.dto.auth.RegisterResponse;
 import com.vinicius.sinance.model.UserEntity;
 import com.vinicius.sinance.dto.user.UserResponse;
 import com.vinicius.sinance.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserEntityRepository userEntityRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserResponse save(UserEntity userEntity) {
         userEntityRepository.save(userEntity);
@@ -19,6 +27,19 @@ public class UserService {
 
     public UserEntity getUserByEmail(String email) {
         return userEntityRepository.getUserEntityByEmail(email);
+    }
+
+    public RegisterResponse register(RegisterRequest registerRequest) {
+        UserEntity newUser = new UserEntity();
+
+        newUser.setUsername(registerRequest.username());
+        newUser.setEmail(registerRequest.email());
+        newUser.setPassword_hash(passwordEncoder.encode(registerRequest.password()));
+        newUser.setCreatedAt(LocalDateTime.now());
+
+        userEntityRepository.save(newUser);
+
+        return new RegisterResponse(newUser.getUsername());
     }
 
 }
